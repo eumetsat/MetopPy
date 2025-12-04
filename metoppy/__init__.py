36372748d1b4ad39ae044398393e4332e8bbd389
lupemba <simon.kok@live.dk>
2025-12-04 13:40:20 +0000

    Enable `with` syntax using the new MetopReader.dataset method.

--- metoppy/__init__.py
+++ metoppy/__init__.py
@@ -11,7 +11,7 @@
 """Metoppy package initialization."""
 
 # Optional: version info
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Import main classes/functions
 from .metopreader import MetopReader
